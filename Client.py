2d886c8c7c5c169cff55be7e9c2df73e60ecbec6
yant <812860165@qq.com>
2019-11-08 16:37:08 +0800

    version 0.0.1

---
+++ Client.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-  
+from socket import socket, AF_INET, SOCK_STREAM
+from threading import Thread
+import curses
+
+class sockClient(object):
+    def __init__(self):
+        self._sock = None
+        self._stdscr = curses.initscr()
+        self._y = 3
+
+    def init(self, host = "localhost", post = 15968):
+        self.set_win()
+        self.connect(host = "localhost", post = 15968)
+        self._stdscr.addstr(1, 0, "connection establish\n", curses.color_pair(1))
+        self._stdscr.refresh()
+
+    def close(self):
+        self.unset_win()
+        self._sock.close()
+
+    def running(self):
+        input_thread = Thread(target = self.input)
+        input_thread.daemon = True
+        input_thread.start()
+
+        print_thread = Thread(target = self.print_message)
+        print_thread.daemon = True
+        print_thread.start()
+        self._stdscr.addstr(2, 0, "Running... ", curses.color_pair(1))
+        self._stdscr.addstr(20, 0, "ME: ", curses.color_pair(1))
+        self._stdscr.refresh()
+        while True:
+            if not input_thread.is_alive() or not print_thread.is_alive():
+                break
+
+    def set_win(self):
+        '''控制台设置'''
+        #使用颜色首先需要调用这个方法
+        curses.start_color()
+        #文字和背景色设置，设置了两个color pair，分别为1和2
+        curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+        curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+        #关闭屏幕回显
+        #设置nodelay，使得控制台可以以非阻塞的方式接受控制台输入，超时1秒
+        self._stdscr.nodelay(1)
+    
+    def unset_win(self):
+        '''控制台重置'''
+        curses.echo()
+        #结束窗口
+        curses.endwin()
+
+    def print_message(self):
+        while True:
+            message = self._sock.recv(1024)
+            if not message:
+                break
+            self._stdscr.addstr(self._y, 0, message, curses.color_pair(1))
+            self._stdscr.addstr(20, 0, "ME: ", curses.color_pair(1))
+            self._stdscr.refresh()
+            self._y += 1
+            self._y = self._y % 15
+
+    def input(self):
+        self._stdscr.nodelay(0)
+        while True:
+            s = self._stdscr.getstr(20, 5)
+            self._stdscr.addstr(20, 5, " "*100, curses.color_pair(1))
+            if s == b"":
+                break
+            self._sock.sendall(s)
+        self._stdscr.nodelay(1)
+
+    def connect(self, host = "localhost", post = 15968):
+        sock = socket(AF_INET, SOCK_STREAM)
+        sock.connect((host, post))
+        self._sock = sock
+
+
+if __name__ == "__main__":
+    client = sockClient()
+    # init
+    client.init()
+
+    # run Loop
+    client.running()
+
+    # close
+    client.close()
---
+++ Server.py
@@ -0,0 +1,61 @@
+from socket import socket, AF_INET, SOCK_STREAM
+from threading import Thread, Condition
+from queue import Queue
+
+class sockServer(object):
+    def __init__(self):
+        self._client_queue = Queue()
+        self._client_name2sock = {}
+        self._client_sock2name = {}
+
+    def init(self, host = "", post = 15968, nworkers = 10):
+        # Run the erver
+        sock = socket(AF_INET,SOCK_STREAM)
+        sock.bind((host, post))
+        sock.listen(5)
+        for i in range(nworkers):
+            t = Thread(target=self.inst_client)
+            t.daemon = True
+            t.start()
+        print("server runing...........")
+        client_name_poisx = "client"
+        index = 1
+        while True:
+            client_sock, client_addr = sock.accept()
+            client_name = client_name_poisx + str(index)
+            index += 1
+            self._client_name2sock[client_name] = client_sock
+            self._client_sock2name[client_sock] = client_name
+            self._client_queue.put((client_sock, client_addr))
+
+    def inst_client(self):
+        sock, client_addr = self._client_queue.get()
+        print("Got Connection from", client_addr)
+        
+        read_thread = Thread(target = self.read, args = (sock, ))
+        read_thread.daemon = True
+        read_thread.start()
+
+        while True:
+            if not read_thread.is_alive():
+                break
+        client_name = self._client_sock2name[sock]
+        del self._client_sock2name[sock]
+        del self._client_name2sock[client_name]
+        print("Connection closed: ", client_addr)
+
+    def read(self, sock):
+        while True:
+            message = sock.recv(1024)
+            client_name = self._client_sock2name[sock]
+            message = str(client_name) + ": " + str(message)
+            message = message.encode()
+            print("message:", message)
+            if message == b"":
+                break
+            for client_sock in self._client_name2sock.values():
+                client_sock.sendall(message)
+        
+if __name__ == "__main__":
+    server = sockServer()
+    server.init()
